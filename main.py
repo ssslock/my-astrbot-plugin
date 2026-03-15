b379bac71aaf1940b7a59347c6ee44fc6496aa72
ssslock <ssslock@mail.ustc.edu.cn>
2026-03-15 05:36:26 -0500

    remove yield

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ class MyPlugin(Star):
     async def on_all_message(self, event: AstrMessageEvent):
         logger.info("setting role")
         event.message_obj.role = event.role # 用户的角色
-        yield event.plain_result("收到了一条消息。")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
